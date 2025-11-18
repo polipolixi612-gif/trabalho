0bbd7ae39844794e1d73e43c46bcc2b41f62cbc5
bryan.py <polipolixi612@gmail.com>
2025-11-18 11:26:26 -0300

    oiii

--- augusto.py
+++ augusto.py
@@ -1,1 +1,45 @@
-print("oi braia")
+import turtle
+tela = turtle.Screen()
+tela.title("Clique para aparecer uma maça")
+tela.bgcolor("White")
+
+t = turtle.Turtle()
+t.hideturtle()
+t.speed(0)
+
+def desenhar_maça(x, y):
+    t.penup()
+    t.goto(x, y - 40)
+    t.pendown()
+
+    t.color("red")
+    t.begin_fill()
+    t.circle(40)
+    t.end_fill()
+    
+    t.penup()
+    t.goto(x, y + 40)
+    t.pendown()
+    t.color("black")
+    t.pensize(5)
+    t.setheading(90)
+    t.forward(20)
+
+    t.color("green")
+    t.begin_fill()
+    t.circle(15, 90)
+    t.left(90)
+    t.circle(15, 90)
+    t.end_fill()
+    
+
+    t.color("green")
+    t.begin_fill()
+    t.circle(15, 90)
+    t.right(90)
+    t.circle(15, 90)
+    t.end_fill()
+    
+tela.onscreenclick(desenhar_maça)
+
+turtle.done()
